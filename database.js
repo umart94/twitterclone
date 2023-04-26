8a894f2928b96d2d8325ff45a512bc658c926916
umart94 <umartariqx94@gmail.com>
2023-04-26 22:35:11 +0500

    initial commit

---
+++ database.js
@@ -0,0 +1,26 @@
+//The require(‘mongoose’) call above returns a Singleton object. 
+//It means that the first time you call require(‘mongoose’), it 
+//is creating an instance of the Mongoose class and returning it. 
+//On subsequent calls, it will return the same instance that was 
+//created and returned to you the first time because of how module 
+//import/export works in ES6.
+const mongoose = require("mongoose");
+
+class Database {
+
+    constructor() {
+        this.connect();
+    }
+
+    connect() {
+        mongoose.connect("GET THIS FROM MONGODB ATLAS")
+            .then(() => {
+                console.log("database connection successful");
+            })
+            .catch((err) => {
+                console.log("database connection error " + err);
+            })
+    }
+}
+
+module.exports = new Database();
